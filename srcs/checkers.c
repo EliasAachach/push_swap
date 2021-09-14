51be55bdaa7695b1bf5be29d4a48baea094542ee
Elias AACHACH <elaachac@e2r4p13.42.fr>
2021-09-14 12:24:38 +0200

    wip

---
+++ srcs/checkers.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checkers.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elaachac <elaachac@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/14 12:23:32 by elaachac          #+#    #+#             */
+/*   Updated: 2021/09/14 12:23:54 by elaachac         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	check_int(char *char_nbr)
+{
+	int i;
+
+	i = 0;
+	ft_atoi_check(char_nbr);
+	while (char_nbr[i])
+	{
+		if (ft_isdigit(char_nbr[i]) == 0)
+			error_prog(0);
+			i++;
+	}
+}
+
+int	dup_check( char **list, char *nbr, int imax)
+{
+	int i;
+	int int_nbr;
+
+	i = 1;
+	int_nbr = ft_atoi(nbr);
+	while (i < imax)
+	{
+		if (int_nbr == ft_atoi(list[i]))
+			return (1);
+			i++;
+	}
+	return (0);
+}
+
+void	check_args(int argc, char **argv)
+{
+	int i;
+
+	i = 1;
+	if (argc <= 2)
+		error_prog(0);
+	while (argv[i])
+	{
+		check_int(argv[i]);
+		if (dup_check(argv, argv[i], i) == 1)
+			error_prog(0);
+		i++;
+	}
+	
+}
--- srcs/push_swap.c
+++ srcs/push_swap.c
@@ -6,60 +6,15 @@
 /*   By: elaachac <elaachac@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 15:56:10 by elaachac          #+#    #+#             */
-/*   Updated: 2021/09/10 16:20:04 by elaachac         ###   ########.fr       */
+/*   Updated: 2021/09/14 12:24:17 by elaachac         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <push_swap.h>
 
-void	check_int(char *char_nbr)
-{
-	int i;
-
-	i = 0;
-	ft_atoi_check(char_nbr);
-	while (char_nbr[i])
-	{
-		if (ft_isdigit(char_nbr[i]) == 0)
-			error_prog(0);
-			i++;
-	}
-}
-
-int	dup_check( char **list, char *nbr, int imax)
-{
-	int i;
-	int int_nbr;
-
-	i = 1;
-	int_nbr = ft_atoi(nbr);
-	while (i < imax)
-	{
-		if (int_nbr == ft_atoi(list[i]))
-			return (1);
-			i++;
-	}
-	return (0);
-}
-
-void	check_args(int argc, char **argv)
-{
-	int i;
-
-	i = 1;
-	if (argc <= 2)
-		error_prog(0);
-	while (argv[i])
-	{
-		check_int(argv[i]);
-		if (dup_check(argv, argv[i], i) == 1)
-			error_prog(0);
-		i++;
-	}
-	
-}
 
 int	main(int argc, char **argv)
 {
 	check_args(argc, argv);
+	
 }
